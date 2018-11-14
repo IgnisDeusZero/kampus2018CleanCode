1f55e3001b8e651da64a235aac9207e6d7861054
Ignis Deus <ignisdeus2048@gmail.com>
2018-11-14 16:45:07 +0500

    snils ready

--- cs/ControlDigit/Snils/SnilsExtensions.cs
+++ cs/ControlDigit/Snils/SnilsExtensions.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ControlDigit
 {
+    
     public static class SnilsExtensions
     {
         public static int CalculateSnils(this long number)
         {
-            throw new NotImplementedException();
+            return number
+                .ToDigitsArray()
+                .GetFactorSum(GetSequence())
+                .GetControlDigit();
+        }
+
+        private static IEnumerable<int> GetSequence()
+        {
+            int i = 1;
+            while (true)
+            {
+                yield return i;
+                i++;
+            }
+        }
+        private static int GetControlDigit(this int sum)
+        {
+            if (sum < 100)
+            {
+                return sum;
+            }
+
+            else if (sum > 101)
+            {
+                return (sum % 101).GetControlDigit();
+            }
+
+            return 0;
         }
     }
 }
--- cs/ControlDigit/Upc/UpcExtensions.cs
+++ cs/ControlDigit/Upc/UpcExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace ControlDigit
 {
@@ -6,7 +9,81 @@ namespace ControlDigit
     {
         public static int CalculateUpc(this long number)
         {
-            throw new NotImplementedException();
+            return number
+                .ToDigitsArray()
+                .GetFactorSum(GetFactors())
+                .GetControlDigit();
+        }
+
+        private static IEnumerable<int> GetFactors()
+        {
+            int i = 3;
+            while (true)
+            {
+                yield return i;
+                i = 4 - i;
+            }
+        }
+
+        private static int GetFactorSum(this long number)
+        {
+            return SumOfDigitsOnEvenPositions(number) * 3 + SumOfDigitsOnEvenPositions(number / 10);
+        }
+
+        private static int SumOfDigitsOnEvenPositions(this long number)
+        {
+            int sum = 0;
+            if (number != 0)
+            {
+                var lastDigit = (int)(number % 10);
+                sum = lastDigit + SumOfDigitsOnEvenPositions(number / 100);
+            }
+            return sum;
+        }
+
+        public static IEnumerable<int> ToDigitsArray(this long number)
+        {
+            return number
+                .ToString()
+                .Select(c => int.Parse(c.ToString()));
+        }
+
+        private static int GetWeightedSum(this IEnumerable<int> digitsArray)
+        {
+            int sum = 0;
+            bool isOdd = true;
+            foreach (int digit in digitsArray.Reverse())
+            {
+                sum += digit * (isOdd ? 3 : 1);
+                isOdd = !isOdd;
+            }
+
+            return sum;
+        }
+
+        public static int GetFactorSum(this IEnumerable<int> digitsArray, IEnumerable<int> factorSource)
+        {
+            int sum = 0;
+            var factor = factorSource.GetEnumerator();
+            foreach (int digit in digitsArray.Reverse())
+            {
+                factor.MoveNext();
+                sum += digit * factor.Current;
+            }
+
+            return sum;
+        }
+
+        private static int GetControlDigit(this int sum)
+        {
+            var lastDigit = sum % 10;
+            int res = 0;
+            if (lastDigit != 0)
+            {
+                res = 10 - lastDigit;
+            }
+
+            return res;
         }
     }
 }
